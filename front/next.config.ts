70456025b1630bebbc7c0f829818a58e99860d11
kirich <kirich@wisebits.com>
2025-11-10 17:28:31 +0200

    ESD-13: fix frontend base url

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material'],
-  experimental: {
-    optimizePackageImports: ['@mui/material', '@mui/icons-material'],
-  },
 };
 
 export default nextConfig;
