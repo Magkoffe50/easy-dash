7b145436bb59a56df1f855be837da3e2fc1c6b17
kirich <kirich@wisebits.com>
2025-11-11 09:38:44 +0200

    ESD-13: fix frontend base url

--- front/src/shared/api/httpRequestService.ts
+++ front/src/shared/api/httpRequestService.ts
@@ -1,12 +1,7 @@
 import { HttpRequestConfig, HttpResult } from './types';
 
 const getApiBaseUrl = (): string => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-  if (!apiUrl) {
-    throw new Error('NEXT_PUBLIC_API_URL is not defined');
-  }
-
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
   return apiUrl;
 };
 
